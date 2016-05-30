d5ff069e94f67d463a2c29d30c8295375aeef868
blaspheme7 <qortkspower@naver.com>
2016-05-30 17:25:22 +0900

    160530 동적 SQL 엘리먼트

--- newset/src/spms/dao/MySqlProjectDao.java
+++ newset/src/spms/dao/MySqlProjectDao.java
@@ -1,10 +1,7 @@
 package spms.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 
 import javax.sql.DataSource;
@@ -30,7 +27,7 @@ public class MySqlProjectDao implements ProjectDao {
 	    this.ds = ds;
 	}
 	
-	public List<Project> selectList() throws Exception {
+	public List<Project> selectList(HashMap<String,Object> paramMap) throws Exception {
 	    //Connection connection = null;
 	    //Statement stmt = null;
 	    //ResultSet rs = null;
@@ -65,7 +62,7 @@ public class MySqlProjectDao implements ProjectDao {
 	      try {if (connection != null) connection.close();} catch(Exception e) {}
 	    }
 	    */
-	    	return sqlSession.selectList("spms.dao.ProjectDao.selectList");
+	    	return sqlSession.selectList("spms.dao.ProjectDao.selectList",paramMap);
 	    } finally {
 	    	sqlSession.close();
 	    }
@@ -115,32 +112,61 @@ public class MySqlProjectDao implements ProjectDao {
 		SqlSession sqlSession=sqlSessionFactory.openSession();
 		
 	    try {
-	    	/*
-	      connection = ds.getConnection();
-	      stmt = connection.prepareStatement(
-	          "UPDATE PROJECTS SET PNAME=?,CONTENT=?,STA_DATE=?,END_DATE=?,STATE=?,TAGS=?"
-	              + " WHERE PNO=?");
-	      stmt.setString(1, project.getTitle());
-	      stmt.setString(2, project.getContent());
-	      stmt.setDate(3, new java.sql.Date(project.getStartDate().getTime()));
-	      stmt.setDate(4, new java.sql.Date(project.getEndDate().getTime()));
-	      stmt.setInt(5, project.getState());
-	      stmt.setString(6, project.getTags());
-	      stmt.setInt(7, project.getNo());
-	      return stmt.executeUpdate();
-
-	    } catch (Exception e) {
-	      throw e;
-
-	    } finally {
-	      try {if (stmt != null) stmt.close();} catch(Exception e) {}
-	      try {if (connection != null) connection.close();} catch(Exception e) {}
-	    }
-	    */
-	    	int count=Integer.valueOf(sqlSession.update("spms.dao.ProjectDao.update", project));
+	    	
+	    	Project original=sqlSession.selectOne("spms.dao.ProjectDao.selectOne",project.getNo());
+	    	
+	    	Hashtable<String,Object> paramMap=new Hashtable<String,Object>();
+	    	if(!project.getTitle().equals(original.getTitle()))
+	    		paramMap.put("title", project.getTitle());
+	    	if(!project.getContent().equals(original.getContent()))
+	    		paramMap.put("content", project.getContent());
+	    	if(project.getStartDate().compareTo(original.getStartDate())!=0)
+	    		paramMap.put("startDate", project.getStartDate());
+	    	if(project.getEndDate().compareTo(original.getEndDate())!=0)
+	    		paramMap.put("startDate", project.getEndDate());
+	    	if(project.getState()!=original.getState())
+	    		paramMap.put("state", project.getState());
+	    	if(!project.getTags().equals(original.getTags()))
+	    		paramMap.put("tags", project.getTags());
+	    	
+	    	if(paramMap.size()>0) {
+	    		
+	    		paramMap.put("no", project.getNo());
+	    		int count=sqlSession.update("spms.dao.ProjectDao.update", paramMap);
+	    		sqlSession.commit();
+	    		return count;
+	    		
+	    	} else { return 0; }
+	    	
+	    	
+	    	
+	    	//int count=Integer.valueOf(sqlSession.update("spms.dao.ProjectDao.update", project));
 	    	//int count=sqlSession.update("spms.dao.ProjectDao.update", project);
-	    	sqlSession.commit();
-	    	return count;
+	    	
+	    	
+	    	
+	    	/*
+		      connection = ds.getConnection();
+		      stmt = connection.prepareStatement(
+		          "UPDATE PROJECTS SET PNAME=?,CONTENT=?,STA_DATE=?,END_DATE=?,STATE=?,TAGS=?"
+		              + " WHERE PNO=?");
+		      stmt.setString(1, project.getTitle());
+		      stmt.setString(2, project.getContent());
+		      stmt.setDate(3, new java.sql.Date(project.getStartDate().getTime()));
+		      stmt.setDate(4, new java.sql.Date(project.getEndDate().getTime()));
+		      stmt.setInt(5, project.getState());
+		      stmt.setString(6, project.getTags());
+		      stmt.setInt(7, project.getNo());
+		      return stmt.executeUpdate();
+
+		    } catch (Exception e) {
+		      throw e;
+
+		    } finally {
+		      try {if (stmt != null) stmt.close();} catch(Exception e) {}
+		      try {if (connection != null) connection.close();} catch(Exception e) {}
+		    }
+		    */
 	    } finally {
 	    	sqlSession.close();
 	    }
